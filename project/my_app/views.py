a6d55d7a8804d33062462c1894e2ee4ae320c79a
Adithyakb2005 <adithyakb1001@gmail.com>
2024-11-01 11:34:02 +0530

    new

--- project/my_app/views.py
+++ project/my_app/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
 def fun1(request):
     return HttpResponse("Hello World")
+l=[]
 def fun2(request):
-    return render(request,'demo.html')
+    if request.method=='POST':
+        name=request.POST['name']
+        age=request.POST['age']
+        print(name,age)
+        l.append({'name':name,'age':age})
+        print(l)
+        return redirect(fun2)
+    else:
+        return render(request,'demo.html')
 def fun3(request):
     return render(request,'about.html')
 def fun4(request):
     return render(request,'contact.html')
+
+l=[{'name':'aa','age':'22'},{'name':'ba','age':'32'}]
+def display(request):
+    a='welcome'
+    return render(request,'display.html',{'data':l,'data1':a})
+
+def add_dtls(request):
+    if request.method=='POST':
+        name=request.POST['name']
+        age=request.POST['age']
+        print(name,age)
+        l.append({'name':name,'age':age})
+        return redirect(display)
+    else:
+        return render(request,'add_dtls.html')
--- stdprj/stdprj/urls.py
+++ stdprj/stdprj/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for project project.
+URL configuration for stdprj project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -16,15 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from my_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('h1',views.fun1),
-    path('demo',views.fun2),
-    path('about',views.fun3),
-    path('contact',views.fun4),
-    path('display',views.display),
-    path('add_dtls',views.add_dtls)
-
 ]
